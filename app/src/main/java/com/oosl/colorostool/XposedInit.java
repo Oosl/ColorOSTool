6087d294299f0e07f9afc55b3b14699706de3dcc
Oosl <meand1206@gmail.com>
2021-08-17 21:21:46 +0800

    解除ColorOS 只允许5个非白名单应用自启动的限制

---
+++ app/src/main/java/com/oosl/colorostool/XposedInit.java
@@ -0,0 +1,50 @@
+package com.oosl.colorostool;
+
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+import java.lang.reflect.Method;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class XposedInit implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if (lpparam.packageName.equals("com.coloros.safecenter")) {
+            Log.d("ColorOSTool","Hook safecenter success!");
+            XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    Class<?> clazz = null;
+                    ClassLoader cl = ((Context) param.args[0]).getClassLoader();
+                    try {
+                        clazz = cl.loadClass("com.coloros.safecenter.startupapp.b");
+                        Log.d("ColorOSTool","Hook safecenter.startupapp.b success!");
+                    } catch (Exception e) {
+                        return;
+                    }
+                    Method[] methods = clazz.getDeclaredMethods();
+                    Method wantMethod = null;
+                    for (int i = 0; i < methods.length; i++) {
+                        if (methods[i].toString().equals("public int com.coloros.safecenter.startupapp.b.v()")){
+                            wantMethod = methods[i];
+                            Log.d("ColorOSTool","Hook startupapp.b.v() success!");
+                        }
+                    }
+                    XposedBridge.hookMethod(wantMethod, new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            XposedHelpers.setIntField(param.thisObject, "b", 114514);
+                            Log.d("ColorOSTool","Hook success! the max startup allowed app is " + XposedHelpers.getIntField(param.thisObject, "b"));
+                        }
+                    });
+                }
+            });
+        }
+    }
+}
