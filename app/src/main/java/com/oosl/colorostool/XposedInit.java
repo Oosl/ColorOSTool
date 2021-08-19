26d505db97353d185bc41f92a5a216db5322be29
Oosl <meand1206@gmail.com>
2021-08-19 12:45:57 +0800

    v1.2：解除启动器只能锁定5个后台应用的限制

--- app/src/main/java/com/oosl/colorostool/XposedInit.java
+++ app/src/main/java/com/oosl/colorostool/XposedInit.java
@@ -12,6 +12,7 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class XposedInit implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+
         if (lpparam.packageName.equals("com.coloros.safecenter")) {
             Log.d("ColorOSTool","Hook safecenter success!");
             XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
@@ -34,6 +35,31 @@ public class XposedInit implements IXposedHookLoadPackage {
                     });
                 }
             });
+        }
+
+        if (lpparam.packageName.equals("com.oppo.launcher")) {
+            Log.d("ColorOSTool","Hook oppoLauncher success!");
+            XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    Class<?> clazz;
+                    ClassLoader cl = ((Context) param.args[0]).getClassLoader();
+                    try {
+                        clazz = cl.loadClass("com.coloros.quickstep.applock.ColorLockManager");
+                        Log.d("ColorOSTool","Hook launcher.quickstep.applock.ColorLockManager success!");
+                    } catch (Exception e) {
+                        return;
+                    }
+                    XposedHelpers.findAndHookConstructor(clazz, Context.class, new XC_MethodHook() {
+                        @Override
+                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                            XposedHelpers.setIntField(param.thisObject,"mLockAppLimit",114514);
+                            Log.d("ColorOSTool","Hook launcher.~.ColorLockManager Constructor success!");
+                        }
+                    });
+                }
+            });
+
         }
     }
 }
