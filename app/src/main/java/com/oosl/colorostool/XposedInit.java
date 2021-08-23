a2d73e8bc46583579c250b4ade6b8219170b8965
Oosl <meand1206@gmail.com>
2021-08-23 20:47:40 +0800

    change the method of hooking safecenter from XC_MethodHook to XC_MethodReplacement

--- app/src/main/java/com/oosl/colorostool/XposedInit.java
+++ app/src/main/java/com/oosl/colorostool/XposedInit.java
@@ -6,13 +6,13 @@ import android.util.Log;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class XposedInit implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-
         if (lpparam.packageName.equals("com.coloros.safecenter")) {
             Log.d("ColorOSTool","Hook safecenter success!");
             XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
@@ -26,19 +26,21 @@ public class XposedInit implements IXposedHookLoadPackage {
                     } catch (Exception e) {
                         return;
                     }
-                    XposedHelpers.findAndHookMethod(clazz, "c", Context.class, new XC_MethodHook() {
+                    XposedHelpers.findAndHookMethod(clazz, "c", Context.class, new XC_MethodReplacement() {
                         @Override
-                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                            if(param == null)
+                                return null;
                             XposedHelpers.setStaticIntField(clazz, "b", 114514);
-                            Log.d("ColorOSTool","After Hook c ! the max startup allowed app is " + XposedHelpers.getStaticIntField(clazz, "b"));
+                            Log.d("StartupManager", "update max allow count ? " + 114514);
+                            Log.d("ColorOSTool", "After Hook c ! the max startup allowed app is " + XposedHelpers.getStaticIntField(clazz, "b"));
+                            return null;
                         }
                     });
                 }
             });
-        }
-
-        if (lpparam.packageName.equals("com.oppo.launcher")) {
-            Log.d("ColorOSTool","Hook oppoLauncher success!");
+        }else if (lpparam.packageName.equals("com.oppo.launcher")) {
+            Log.d("ColorOSTool", "Hook oppoLauncher success!");
             XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
                 @Override
                 protected void afterHookedMethod(MethodHookParam param) throws Throwable {
@@ -46,20 +48,19 @@ public class XposedInit implements IXposedHookLoadPackage {
                     ClassLoader cl = ((Context) param.args[0]).getClassLoader();
                     try {
                         clazz = cl.loadClass("com.coloros.quickstep.applock.ColorLockManager");
-                        Log.d("ColorOSTool","Hook launcher.quickstep.applock.ColorLockManager success!");
+                        Log.d("ColorOSTool", "Hook launcher.quickstep.applock.ColorLockManager success!");
                     } catch (Exception e) {
                         return;
                     }
                     XposedHelpers.findAndHookConstructor(clazz, Context.class, new XC_MethodHook() {
                         @Override
                         protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                            XposedHelpers.setIntField(param.thisObject,"mLockAppLimit",114514);
-                            Log.d("ColorOSTool","Hook launcher.~.ColorLockManager Constructor success!");
+                            XposedHelpers.setIntField(param.thisObject, "mLockAppLimit", 114514);
+                            Log.d("ColorOSTool", "Hook launcher.~.ColorLockManager Constructor success!");
                         }
                     });
                 }
             });
-
         }
     }
 }
