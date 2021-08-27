0c3c52eeb62eed30bdcd33a4ec9f7d2ec9006551
Oosl <meand1206@gmail.com>
2021-08-27 15:57:06 +0800

    remove the verification of oppo package installer before install apk.

--- app/src/main/java/com/oosl/colorostool/XposedInit.java
+++ app/src/main/java/com/oosl/colorostool/XposedInit.java
@@ -7,14 +7,17 @@ import android.util.Log;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class XposedInit implements IXposedHookLoadPackage {
+    private static final String TAG = "ColorOSTool";
+
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         if (lpparam.packageName.equals("com.coloros.safecenter")) {
-            Log.d("ColorOSTool","Hook safecenter success!");
+            Log.d(TAG,"Hook safecenter success!");
             XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
                 @Override
                 protected void afterHookedMethod(MethodHookParam param) throws Throwable {
@@ -22,25 +25,25 @@ public class XposedInit implements IXposedHookLoadPackage {
                     ClassLoader cl = ((Context) param.args[0]).getClassLoader();
                     try {
                         clazz = cl.loadClass("com.coloros.safecenter.startupapp.b");
-                        Log.d("ColorOSTool","Hook safecenter.startupapp.b success!");
+                        Log.d(TAG,"Hook safecenter.startupapp.b success!");
+                        XposedHelpers.findAndHookMethod(clazz, "c", Context.class, new XC_MethodReplacement() {
+                            @Override
+                            protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                                if(param == null)
+                                    return null;
+                                XposedHelpers.setStaticIntField(clazz, "b", 114514);
+                                Log.d("StartupManager", "update max allow count ? " + 114514);
+                                Log.d(TAG, "After Hook c ! the max startup allowed app is " + XposedHelpers.getStaticIntField(clazz, "b"));
+                                return null;
+                            }
+                        });
                     } catch (Exception e) {
                         return;
                     }
-                    XposedHelpers.findAndHookMethod(clazz, "c", Context.class, new XC_MethodReplacement() {
-                        @Override
-                        protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-                            if(param == null)
-                                return null;
-                            XposedHelpers.setStaticIntField(clazz, "b", 114514);
-                            Log.d("StartupManager", "update max allow count ? " + 114514);
-                            Log.d("ColorOSTool", "After Hook c ! the max startup allowed app is " + XposedHelpers.getStaticIntField(clazz, "b"));
-                            return null;
-                        }
-                    });
                 }
             });
         }else if (lpparam.packageName.equals("com.oppo.launcher")) {
-            Log.d("ColorOSTool", "Hook oppoLauncher success!");
+            Log.d(TAG, "Hook oppoLauncher success!");
             XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
                 @Override
                 protected void afterHookedMethod(MethodHookParam param) throws Throwable {
@@ -48,7 +51,7 @@ public class XposedInit implements IXposedHookLoadPackage {
                     ClassLoader cl = ((Context) param.args[0]).getClassLoader();
                     try {
                         clazz = cl.loadClass("com.coloros.quickstep.applock.ColorLockManager");
-                        Log.d("ColorOSTool", "Hook launcher.quickstep.applock.ColorLockManager success!");
+                        Log.d(TAG, "Hook launcher.quickstep.applock.ColorLockManager success!");
                     } catch (Exception e) {
                         return;
                     }
@@ -56,9 +59,20 @@ public class XposedInit implements IXposedHookLoadPackage {
                         @Override
                         protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                             XposedHelpers.setIntField(param.thisObject, "mLockAppLimit", 114514);
-                            Log.d("ColorOSTool", "Hook launcher.~.ColorLockManager Constructor success!");
+                            Log.d(TAG, "Hook launcher.~.ColorLockManager Constructor success!");
                         }
                     });
+                }
+            });
+        }else if (lpparam.packageName.equals("com.android.packageinstaller")) {
+            Log.d(TAG, "Hook packageinstaller success!");
+            Class<?> clazz = lpparam.classLoader.loadClass("com.android.packageinstaller.oplus.OPlusPackageInstallerActivity");
+            XposedHelpers.findAndHookMethod(clazz, "continueOppoSafeInstall", new XC_MethodReplacement() {
+                @Override
+                protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                    Log.d(TAG, "replace OppoSafeInstall OK!!");
+                    XposedHelpers.callMethod(param.thisObject,"continueAppInstall");
+                    return null;
                 }
             });
         }
