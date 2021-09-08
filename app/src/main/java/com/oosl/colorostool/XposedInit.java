fc170a7dbad1a3bc968ad393e49a70f2c7a9ffa3
Oosl <meand1206@gmail.com>
2021-09-08 16:57:40 +0800

    code optimizations

--- app/src/main/java/com/oosl/colorostool/XposedInit.java
+++ app/src/main/java/com/oosl/colorostool/XposedInit.java
@@ -1,5 +1,6 @@
 package com.oosl.colorostool;
 
+import android.annotation.SuppressLint;
 import android.app.Application;
 import android.content.Context;
 import android.util.Log;
@@ -18,93 +19,24 @@ public class XposedInit implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         if (lpparam.packageName.equals("com.coloros.safecenter") && prefs.getBoolean("startup", true)) {
-            //去除只能开启5个应用自启动的限制
-            ColorOSToolLog("Hook safecenter success!");
-            XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
-                @Override
-                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                Class<?> clazz;
-                ClassLoader cl = ((Context) param.args[0]).getClassLoader();
-                try {
-                    clazz = cl.loadClass("com.coloros.safecenter.startupapp.b");
-                    ColorOSToolLog("Hook safecenter.startupapp.b success!");
-                } catch (Exception e) {
-                    return;
-                }
-                XposedHelpers.findAndHookMethod(clazz, "c", Context.class, new XC_MethodReplacement() {
-                    @Override
-                    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-                        if(param == null)
-                            return null;
-                        XposedHelpers.setStaticIntField(clazz, "b", 114514);
-                        Log.d("StartupManager", "update max allow count ? " + 114514);
-                        ColorOSToolLog("After Hook c ! the max startup allowed app is " + XposedHelpers.getStaticIntField(clazz, "b"));
-                        return null;
-                    }
-                });
-                }
-            });
+            hookSafeCenter();
         }else if (lpparam.packageName.equals("com.oppo.launcher") && prefs.getBoolean("app_lock", true)) {
-            // 去除多任务后台只能锁定5个的限制
-            ColorOSToolLog("Hook oppoLauncher success!");
-            XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
-                @Override
-                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                Class<?> clazz;
-                ClassLoader cl = ((Context) param.args[0]).getClassLoader();
-                try {
-                    clazz = cl.loadClass("com.coloros.quickstep.applock.ColorLockManager");
-                    ColorOSToolLog("Hook launcher.quickstep.applock.ColorLockManager success!");
-                } catch (Exception e) {
-                    return;
-                }
-                XposedHelpers.findAndHookConstructor(clazz, Context.class, new XC_MethodHook() {
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        XposedHelpers.setIntField(param.thisObject, "mLockAppLimit", 114514);
-                        ColorOSToolLog("Hook launcher.~.ColorLockManager Constructor success!");
-                    }
-                });
-                }
-            });
+            hookOppoLuncher();
         }else if (lpparam.packageName.equals("com.android.packageinstaller")) {
-            ColorOSToolLog("Hook packageinstaller success!");
-            // 去除安装前的验证
-            if(prefs.getBoolean("safe_installer", true)) {
-                Class<?> clazz;
-                clazz = lpparam.classLoader.loadClass("com.android.packageinstaller.oplus.OPlusPackageInstallerActivity");
-                XposedHelpers.findAndHookMethod(clazz, "continueOppoSafeInstall", new XC_MethodReplacement() {
-                    @Override
-                    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-                        ColorOSToolLog("replace OppoSafeInstall() OK!!");
-                        XposedHelpers.callMethod(param.thisObject,"continueAppInstall");
-                        return null;
-                    }
-                });
-            }
-            // 使用原生安装器而非OPPO自己写的, false暂时禁用
-            if(prefs.getBoolean("aosp_installer", false)) {
-                Class<?> clazz2;
-                clazz2 = lpparam.classLoader.loadClass("com.android.packageinstaller.oplus.common.FeatureOption");
-                ColorOSToolLog("sIsClosedSuperFirewall is " + XposedHelpers.getStaticBooleanField(clazz2, "sIsClosedSuperFirewall"));
-                XposedHelpers.findAndHookMethod(clazz2, "setIsClosedSuperFirewall", Context.class, new XC_MethodHook() {
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        XposedHelpers.setStaticBooleanField(clazz2, "sIsClosedSuperFirewall", true);
-                        ColorOSToolLog("after sIsClosedSuperFirewall is " + XposedHelpers.getStaticBooleanField(clazz2, "sIsClosedSuperFirewall"));
-                    }
-                });
-            }
+            hookPackageInstaller(lpparam);
         }else if(lpparam.packageName.equals("com.coloros.gamespace") && prefs.getBoolean("root_checker", true)) {
             hookGameSpace(lpparam);
         }
     }
 
-    private static void ColorOSToolLog(String str){
+    /**
+     * auto add TAG to logMesg which will be printed by XposedBridge.log()
+     * @param logMesg
+     * @param tag tag for hook app
+     */
+    private static void ColorOSToolLog(String tag, String logMesg){
         final String TAG = "ColorOSTool";
-
-        XposedBridge.log(TAG + ": " + str);
-        //Log.d(TAG,str);
+        XposedBridge.log(TAG + "-"+ tag + " " + logMesg);
     }
 
     /**
@@ -113,7 +45,8 @@ public class XposedInit implements IXposedHookLoadPackage {
      * @throws ClassNotFoundException
      */
     private  void hookGameSpace(final XC_LoadPackage.LoadPackageParam lpparam) throws ClassNotFoundException {
-        ColorOSToolLog("Hook gamespace success!");
+        String tag = "GameSpace";
+        ColorOSToolLog(tag,"Hook gamespace success!");
         Class<?> clazz;
         clazz = lpparam.classLoader.loadClass("com.oplus.cosa.c.i.f");
         XposedHelpers.findAndHookMethod(clazz, "c", new XC_MethodReplacement() {
@@ -122,7 +55,7 @@ public class XposedInit implements IXposedHookLoadPackage {
                 return true;
             }
         });
-        ColorOSToolLog("Hook gamespace.root.checker success!");
+        ColorOSToolLog(tag,"Hook gamespace.root.checker success!");
     }
 
     /**
@@ -146,16 +79,105 @@ public class XposedInit implements IXposedHookLoadPackage {
      * print StackTrace in XposedBridge-log
      */
     private void PrStackTrace(){
-        ColorOSToolLog("Dump Stack:---------------start----------------");
+        String tag = "ST";
+        ColorOSToolLog(tag,"Dump Stack:---------------start----------------");
         Throwable ex = new Throwable();
         StackTraceElement[] stackElements = ex.getStackTrace();
         for (int i =0; i< stackElements.length; i++){
-            ColorOSToolLog(
+            ColorOSToolLog(tag,
             i + ":"+ stackElements[i].getMethodName()
                 +" in "+stackElements[i].getFileName()
                 +":"+stackElements[i].getLineNumber()
                 +" -> "+stackElements[i].getClassName());
         }
-        ColorOSToolLog("Dump Stack:---------------over----------------");
+        ColorOSToolLog(tag,"Dump Stack:---------------over----------------");
+    }
+
+    private void hookSafeCenter() {
+        String tag = "SafeCenter";
+        //去除只能开启5个应用自启动的限制
+        ColorOSToolLog(tag,"Hook safecenter success!");
+        XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                Class<?> clazz;
+                ClassLoader cl = ((Context) param.args[0]).getClassLoader();
+                try {
+                    clazz = cl.loadClass("com.coloros.safecenter.startupapp.b");
+                    ColorOSToolLog(tag,"Hook safecenter startupapp success!");
+                } catch (Exception e) {
+                    return;
+                }
+                XposedHelpers.findAndHookMethod(clazz, "c", Context.class, new XC_MethodReplacement() {
+                    @Override
+                    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                        if(param == null)
+                            return null;
+                        XposedHelpers.setStaticIntField(clazz, "b", 114514);
+                        Log.d("StartupManager", "update max allow count ? " + 114514);
+                        ColorOSToolLog(tag,"After Hook! the max startup allowed app is " + XposedHelpers.getStaticIntField(clazz, "b"));
+                        return null;
+                    }
+                });
+            }
+        });
+    }
+
+    private void hookOppoLuncher() {
+        String tag = "OppoLuncher";
+        // 去除多任务后台只能锁定5个的限制
+        ColorOSToolLog(tag,"Hook oppoLauncher success!");
+        XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                Class<?> clazz;
+                ClassLoader cl = ((Context) param.args[0]).getClassLoader();
+                try {
+                    clazz = cl.loadClass("com.coloros.quickstep.applock.ColorLockManager");
+                    ColorOSToolLog(tag,"Hook launcher app_lock success!");
+                } catch (Exception e) {
+                    return;
+                }
+                XposedHelpers.findAndHookConstructor(clazz, Context.class, new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedHelpers.setIntField(param.thisObject, "mLockAppLimit", 114514);
+                        ColorOSToolLog(tag,"Hook app_lock to 114514 successfully!");
+                    }
+                });
+            }
+        });
+    }
+
+    @SuppressLint("PrivateApi")
+    private void hookPackageInstaller(XC_LoadPackage.LoadPackageParam lpparam) throws ClassNotFoundException {
+        String tag = "PackageInstaller";
+        ColorOSToolLog(tag,"Hook packageinstaller success!");
+        // 去除安装前的验证
+        if(prefs.getBoolean("safe_installer", true)) {
+            Class<?> clazz;
+            clazz = lpparam.classLoader.loadClass("com.android.packageinstaller.oplus.OPlusPackageInstallerActivity");
+            XposedHelpers.findAndHookMethod(clazz, "continueOppoSafeInstall", new XC_MethodReplacement() {
+                @Override
+                protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                    ColorOSToolLog(tag,"replace OppoSafeInstall() OK!!");
+                    XposedHelpers.callMethod(param.thisObject,"continueAppInstall");
+                    return null;
+                }
+            });
+        }
+        // 使用原生安装器而非OPPO自己写的, false暂时禁用
+        if(prefs.getBoolean("aosp_installer", false)) {
+            Class<?> clazz;
+            clazz = lpparam.classLoader.loadClass("com.android.packageinstaller.oplus.common.FeatureOption");
+            ColorOSToolLog(tag,"sIsClosedSuperFirewall is " + XposedHelpers.getStaticBooleanField(clazz, "sIsClosedSuperFirewall"));
+            XposedHelpers.findAndHookMethod(clazz, "setIsClosedSuperFirewall", Context.class, new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    XposedHelpers.setStaticBooleanField(clazz, "sIsClosedSuperFirewall", true);
+                    //ColorOSToolLog(tag,"after sIsClosedSuperFirewall is " + XposedHelpers.getStaticBooleanField(clazz, "sIsClosedSuperFirewall"));
+                }
+            });
+        }
     }
 }
