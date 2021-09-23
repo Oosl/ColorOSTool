85abcbf62a32debeda3c7691c3b10bfcfb6ff2f9
Oosl <meand1206@gmail.com>
2021-09-23 17:39:06 +0800

    move activity class to activity package

--- app/src/main/java/com/oosl/colorostool/activity/SettingsActivity.java
+++ app/src/main/java/com/oosl/colorostool/activity/SettingsActivity.java
@@ -1,4 +1,4 @@
-package com.oosl.colorostool;
+package com.oosl.colorostool.activity;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
@@ -8,6 +8,8 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
+
+import com.oosl.colorostool.R;
 
 public class SettingsActivity extends AppCompatActivity {
 
