0a9c4b89dca1c09aaff4a8e0b6671b10d0d9a83a
Oosl <meand1206@gmail.com>
2021-09-26 15:25:27 +0800

    hooking SafeCenter allowMaxStartup has been adapted to COS12

--- app/src/main/java/com/oosl/colorostool/plugin/HookSafeCenter.java
+++ app/src/main/java/com/oosl/colorostool/plugin/HookSafeCenter.java
@@ -13,6 +13,23 @@ import de.robv.android.xposed.XposedHelpers;
 
 public class HookSafeCenter extends HookBase {
 
+    private static final String tag = "SafeCenter";
+    private static final String className;
+    private static final String methodName;
+    private static final String maxStartup;
+    static {
+        if(CosApkName.isCos12()) {
+            className = "com.oplus.safecenter.startupapp.a";
+            methodName = "b";
+            maxStartup = "d";
+        }else{
+            className = "com.coloros.safecenter.startupapp.b";
+            methodName = "c";
+            maxStartup = "b";
+        }
+    }
+
+
     @Override
     public void hook() {
         super.hook();
@@ -22,7 +39,6 @@ public class HookSafeCenter extends HookBase {
     }
 
     private void hookSafeCenter() {
-        String tag = "SafeCenter";
         //去除只能开启5个应用自启动的限制
         Log.d(tag, "Hook safecenter success!");
         XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
@@ -31,23 +47,24 @@ public class HookSafeCenter extends HookBase {
                 Class<?> clazz;
                 ClassLoader cl = ((Context) param.args[0]).getClassLoader();
                 try {
-                    clazz = cl.loadClass("com.oplus.safecenter.startupapp.a");
-                    Log.d(tag,"Hook safecenter startupapp success!");
+                    clazz = cl.loadClass(className);
+                    Log.d(tag, "Hook safecenter startupapp success!");
                 } catch (Exception e) {
                     return;
                 }
-                XposedHelpers.findAndHookMethod(clazz, "b", Context.class, new XC_MethodReplacement() {
+                XposedHelpers.findAndHookMethod(clazz, methodName, Context.class, new XC_MethodReplacement() {
                     @Override
                     protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-                        if(param == null)
-                            return null;
-                        XposedHelpers.setStaticIntField(clazz, "d", 114514);
-                        android.util.Log.d("StartupManager", "update max allow count ? " + 114514);
-                        Log.d(tag,"After Hook! the max startup allowed app is " + XposedHelpers.getStaticIntField(clazz, "b"));
+                    if (param == null)
                         return null;
+                    XposedHelpers.setStaticIntField(clazz, maxStartup, 114514);
+                    android.util.Log.d("StartupManager", "update max allow count ? " + 114514);
+                    Log.d(tag, "After Hook! the max startup allowed app is " + XposedHelpers.getStaticIntField(clazz, maxStartup));
+                    return null;
                     }
                 });
             }
+
         });
     }
 }
