c498819f55dc1ee702d8b680b8c1b45b800f2d58
Oosl <meand1206@gmail.com>
2021-10-15 16:18:08 +0800

    code optimizations

--- app/src/main/java/com/oosl/colorostool/util/ColorToolPrefs.java
+++ app/src/main/java/com/oosl/colorostool/util/ColorToolPrefs.java
@@ -5,11 +5,7 @@ import com.oosl.colorostool.BuildConfig;
 import de.robv.android.xposed.XSharedPreferences;
 
 public final class ColorToolPrefs {
-    private static XSharedPreferences prefs = new XSharedPreferences(BuildConfig.APPLICATION_ID, "ColorToolPrefs");
-
-    private ColorToolPrefs(){
-
-    }
+    private static final XSharedPreferences prefs = new XSharedPreferences(BuildConfig.APPLICATION_ID, "ColorToolPrefs");
 
     public static boolean getPrefs(String prefsName, Boolean defaultValue){
         //Log.d("prefs:", prefsName + ": " + prefs.getBoolean(prefsName, defaultValue));
