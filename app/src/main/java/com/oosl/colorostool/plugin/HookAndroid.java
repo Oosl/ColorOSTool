b6b09fc60af4371b6589d6d3103a395b8e12655f
liuchun <chun.liu@snowballtech.com>
2021-10-18 20:59:30 +0800

    去掉ColorOS11兼容性支持

--- app/src/main/java/com/oosl/colorostool/plugin/HookAndroid.java
+++ app/src/main/java/com/oosl/colorostool/plugin/HookAndroid.java
@@ -1,9 +1,6 @@
 package com.oosl.colorostool.plugin;
 
-import android.annotation.SuppressLint;
-
 import com.oosl.colorostool.util.ColorToolPrefs;
-import com.oosl.colorostool.util.CosApkName;
 import com.oosl.colorostool.util.Log;
 
 import de.robv.android.xposed.XC_MethodHook;
@@ -27,13 +24,7 @@ public class HookAndroid extends HookBase{
     public void usbInstall(XC_LoadPackage.LoadPackageParam lpparam){
         ClassLoader classLoader = lpparam.classLoader;
         Log.d(tag,"Patch coloros usb alert START");
-        String className;
-        if (CosApkName.isCos12()){
-            className = "com.android.server.pm.OplusPackageInstallInterceptManager";
-        }else {
-            className = "com.android.server.pm.ColorPackageInstallInterceptManager";
-        }
-        XposedHelpers.findAndHookMethod(className, classLoader, "allowInterceptAdbInstallInInstallStage", int.class, "android.content.pm.PackageInstaller$SessionParams", "java.io.File", "java.lang.String", "android.content.pm.IPackageInstallObserver2", new XC_MethodHook() {
+        XposedHelpers.findAndHookMethod("com.android.server.pm.OplusPackageInstallInterceptManager", classLoader, "allowInterceptAdbInstallInInstallStage", int.class, "android.content.pm.PackageInstaller$SessionParams", "java.io.File", "java.lang.String", "android.content.pm.IPackageInstallObserver2", new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
                 super.beforeHookedMethod(param);
