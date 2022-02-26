ea382061751d955197c3c83f2408a7519fd4fa88
Oosl <meand1206@gmail.com>
2022-02-26 23:36:57 +0800

    Adaptation of 8efbcbb of Safe Center

--- app/src/main/java/com/oosl/colorostool/plugin/HookSafeCenter.java
+++ app/src/main/java/com/oosl/colorostool/plugin/HookSafeCenter.java
@@ -46,27 +46,33 @@ public class HookSafeCenter extends HookBase {
                         funName[0] = "c";
                         fieldName[0] = "b";
                         break;
+                    case "8efbcbb":
+                        className[0] ="com.coloros.safecenter.startupapp.b";
+                        funName[0] = "c";
+                        fieldName[0] = "b";
+                        break;
                     default:
                         className[0] = "com.oplus.safecenter.startupapp.a";
+//                        search -> "update max allow count ? " -5
                         funName[0] = "b";
+//                        search -> "update max allow count ? " -1
                         fieldName[0] = "d";
                 }
                 try {
                     clazz = cl.loadClass(className[0]);
-                    Log.d(tag, "Hook safecenter startupapp success!");
-
-                    XposedHelpers.findAndHookMethod(clazz, funName[0], Context.class, new XC_MethodReplacement() {
+                    XposedHelpers.findAndHookMethod(clazz, funName[0], Context.class, new XC_MethodHook() {
                         @Override
-                        protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-                        if (param == null)
-                            return null;
-                        XposedHelpers.setStaticIntField(clazz, fieldName[0], 114514);
-                        android.util.Log.d("StartupManager", "update max allow count ? " + 114514);
-                        Log.d(tag, "After Hook! the max startup allowed app is " + XposedHelpers.getStaticIntField(clazz, "d"));
-                        return null;
+                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                            super.afterHookedMethod(param);
+                            XposedHelpers.setStaticIntField(clazz, fieldName[0], 114514);
+                            android.util.Log.d("StartupManager", "update max allow count ? " + 114514);
+                            Log.d(tag, "After Hook! the max startup allowed app is " + XposedHelpers.getStaticIntField(clazz, "d"));
+
                         }
                     });
+                    Log.d(tag, "Hook safecenter startupapp success!");
                 } catch (Exception e) {
+                    Log.error(tag, e);
                     return;
                 }
             }
