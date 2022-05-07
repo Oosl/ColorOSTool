c431b3287095be87ac7581f35a7a04d04a2fb189
Oosl <meand1206@gmail.com>
2022-05-07 17:38:17 +0800

    [Android] Unrestrict some default applications

--- app/src/main/java/com/oosl/colorostool/plugin/HookAndroid.java
+++ app/src/main/java/com/oosl/colorostool/plugin/HookAndroid.java
@@ -1,5 +1,7 @@
 package com.oosl.colorostool.plugin;
 
+import android.content.ComponentName;
+
 import com.oosl.colorostool.plugin.base.HookBase;
 import com.oosl.colorostool.util.ColorToolPrefs;
 import com.oosl.colorostool.util.Log;
@@ -7,6 +9,7 @@ import com.oosl.colorostool.util.Log;
 import java.io.File;
 
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
@@ -17,14 +20,18 @@ public class HookAndroid extends HookBase {
     @Override
     public void hook(XC_LoadPackage.LoadPackageParam lpparam) {
         super.hook(lpparam);
+        ClassLoader classLoader = lpparam.classLoader;
         if (ColorToolPrefs.getPrefs("usb_install", false)) {
-            usbInstall(lpparam);
+            removeForceApp(classLoader);
+        }
+        if (ColorToolPrefs.getPrefs("remove_oppo_default_app", false)) {
+            removeForceApp(classLoader);
         }
         Log.d(tag, "Hook android success!");
     }
 
-    public void usbInstall(XC_LoadPackage.LoadPackageParam lpparam) {
-        ClassLoader classLoader = lpparam.classLoader;
+    public void usbInstall(ClassLoader classLoader) {
+
         Log.d(tag, "Patch coloros usb alert START");
         XposedHelpers.findAndHookMethod("com.android.server.pm.OplusPackageInstallInterceptManager", classLoader, "allowInterceptAdbInstallInInstallStage", int.class, "android.content.pm.PackageInstaller$SessionParams", File.class, String.class, "android.content.pm.IPackageInstallObserver2", new XC_MethodHook() {
             @Override
@@ -34,5 +41,16 @@ public class HookAndroid extends HookBase {
             }
         });
         Log.d(tag, "Patch coloros usb alert END");
+    }
+
+    public void removeForceApp(ClassLoader classLoader){
+        Log.d(tag, "Patch coloros removeForceApp START");
+        XposedHelpers.findAndHookMethod("com.android.server.pm.OplusOsPackageManagerHelper", classLoader, "isOplusForceApp", ComponentName.class, new XC_MethodReplacement() {
+            @Override
+            protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                return false;
+            }
+        });
+        Log.d(tag, "Patch coloros removeForceApp END");
     }
 }
